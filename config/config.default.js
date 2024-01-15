0defed2d3c086b09e3903f16718b437f67a6a639
gintonic <gintonic@git.com>
2024-01-15 22:28:28 +0800

    fix: 修改端口

--- config/config.default.js
+++ config/config.default.js
@@ -53,6 +53,14 @@ module.exports = appInfo => {
         },
     };
 
+    config.cluster = {
+        listen: {
+            path: '',
+            port: 80,
+            hostname: '0.0.0.0',
+        },
+    };
+
     return {
         ...config,
         ...userConfig,
